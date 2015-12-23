e19cae0a228a9a56121fe925c7f303b79a22869f
sveri <sveri80@gmail.com>
2015-12-23 23:47:37 +0100

    ok

--- src/clj/de/sveri/stockfighter/api/lvl_three.clj
+++ src/clj/de/sveri/stockfighter/api/lvl_three.clj
@@ -2,18 +2,25 @@
   (:require [de.sveri.stockfighter.api.calculations :as calc]
             [schema.core :as s]
             [de.sveri.stockfighter.schema-api :as schem]
-            [de.sveri.stockfighter.api.api :as api]))
+            [de.sveri.stockfighter.api.api :as api]
+            [immutant.scheduling :refer :all]))
 
 
 (def sell-me (atom []))
 
 (s/defn bought-something? :- s/Bool [order :- schem/order] (< 0 (:totalFilled order)))
 
+(def fooo (atom [1 2 3 4]))
+
+;(defn sell-lower-thingy [a]
+;  (swap! a
+;         (fn [v] (mapv #(if) v))))
+
 (s/defn generate-sell-order :- [schem/new-order] [venue :- s/Str stock :- s/Str account :- s/Str order-response :- schem/order]
   (mapv (fn [fill]
-         {:account   account :venue venue :stock stock :price (int (+ (:price fill) (* 0.05 (:price fill))))
+         {:account   account :venue venue :stock stock :price (int (+ (:price fill) (* 0.01 (:price fill))))
           :qty       (:qty fill) :direction "sell"
-          :orderType "fill-or-kill"})
+          :orderType "immediate-or-cancel"})
        (:fills order-response)))
 
 (defn buy-a-thing [venue stock account quote-history]
@@ -30,13 +37,17 @@
   (let [sell-order (first @sell-me)
         sell-response (api/new-order sell-order)]
     (println "trying to sell: " sell-order)
-    (when (= (:qty sell-order) (:totalFilled sell-response))
-      (println "sold: " sell-response)
-      (swap! sell-me (fn [v] (subvec v 1))))))
+    (println sell-response)
+    ;(if (= (:qty sell-order) (:totalFilled sell-response))
+    ;  (do (println "sold: " sell-response)
+    ;      (swap! sell-me (fn [v] (subvec v 1))))
+    ;  (swap! sell-me (fn [v] (let [cur-price (:price sell-order)]
+    ;                           (assoc v 0 (assoc sell-order :price (- cur-price (* cur-price 0.03))))))))
+    ))
 
 (s/defn start-lvl-three :- s/Any
   [{:keys [venue stock account] :as vsa} :- schem/vsa quote-history :- s/Any]
-  (if (< (count @sell-me) 3)
+  (if (empty? @sell-me)
+  ;(if (< (count @sell-me) 3)
     (buy-a-thing venue stock account quote-history)
-    (sell-a-thing)
-    ))
+    (sell-a-thing)))
