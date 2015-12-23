ac9fe52e5f611ec5bb80bb179a8ddf032d1e42a1
sveri <sveri80@gmail.com>
2015-12-23 12:25:48 +0100

    enter lvl three

--- src/clj/de/sveri/stockfighter/api/lvl_three.clj
+++ src/clj/de/sveri/stockfighter/api/lvl_three.clj
@@ -1,1 +1,8 @@
-(ns de.sveri.stockfighter.api.lvl-three)
+(ns de.sveri.stockfighter.api.lvl-three
+  (:require [de.sveri.stockfighter.api.websockets :as api-ws]
+            [schema.core :as s]
+            [de.sveri.stockfighter.schema-api :as schem]))
+
+(s/defn start-lvl-three :- s/Any [{:keys [venue stock account] :as vsa} :- schem/vsa]
+  (let [new-buy (- (api-ws/get-avg-bid venue stock account 5) 50)
+        new-sell (+ (api-ws/get-avg-bid venue stock account 5) 50)]))
